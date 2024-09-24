a90b676f833e77686d0b5c0886090585324ac03d
Michelle Qiu <michelleqiu05@gmail.com>
2024-09-24 16:43:12 -0400

    deleted some wonky stuff

--- michelle.py
+++ michelle.py
@@ -1,9 +1,5 @@
-import os
 from dash import Dash, html, dcc, Input, Output, State
-import pandas as pd
-from openpyxl import load_workbook
-import io
-#test
+
 app = Dash(__name__, suppress_callback_exceptions=True)
 
 app.layout = html.Div(children=[
@@ -32,9 +28,6 @@ app.layout = html.Div(children=[
     html.Div(id='notes-section', style={'display': 'none'}),  # Initially hidden
 
     html.Button('Clear', id='clear-button', style={'margin-top': '20px'}),
-    html.Button('Export to Excel', id='export-button', n_clicks=0),
-    dcc.Download(id="download-dataframe-xlsx"),
-    html.Div(id='export-status', children='Click the button to export data to Excel.')
 ])
 
 
@@ -66,6 +59,7 @@ def toggle_notes_section(n_clicks, style):
     [Input('general-button', 'n_clicks'),
      Input('notes-button', 'n_clicks')],
 )
+
 def update_sections(general_clicks, notes_clicks):
     general_content = html.Div([
         html.Div('Session Number', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
@@ -112,61 +106,6 @@ def update_sections(general_clicks, notes_clicks):
 def clear_inputs(n_clicks):
     if n_clicks:
         return '', None, '', '', '', '', '', '', '', ''
-
-
-@app.callback(
-    [Output('export-status', 'children'), Output('download-dataframe-xlsx', 'data')],
-    Input('export-button', 'n_clicks'),
-    [State('session', 'value'), State('date-picker', 'date'), State('venue', 'value'),
-     State('event', 'value'), State('driver', 'value'), State('weight', 'value'),
-     State('driver-notes', 'value'), State('faults', 'value'),
-     State('improvements', 'value'), State('misc-notes', 'value')]
-)
-def export_to_excel(n_clicks, session, date, venue, event, driver, weight, driver_notes, faults, improvements,
-                    misc_notes):
-    if n_clicks > 0:
-        # Collect data
-        data = {
-            'Session': [session],
-            'Date': [date],
-            'Venue': [venue],
-            'Event': [event],
-            'Driver': [driver],
-            'Weight': [weight],
-            'Driver Notes': [driver_notes],
-            'Faults': [faults],
-            'Improvements': [improvements],
-            'Misc Notes': [misc_notes]
-        }
-
-        # Create DataFrame
-        df = pd.DataFrame(data)
-
-        # Define file path
-        filename = 'exported_data.xlsx'
-        filepath = os.path.join(os.getcwd(), filename)
-
-        if os.path.exists(filepath):
-            # If file exists, append data to it without overwriting
-            book = load_workbook(filepath)
-            with pd.ExcelWriter(filepath, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
-                df.to_excel(writer, sheet_name='Sheet1', startrow=writer.sheets['Sheet1'].max_row, index=False,
-                            header=False)
-        else:
-            # If the file doesn't exist, create a new one
-            with pd.ExcelWriter(filepath, engine='openpyxl') as writer:
-                df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-        # Create a downloadable version for user
-        stream = io.BytesIO()
-        with pd.ExcelWriter(stream, engine='openpyxl') as writer:
-            df.to_excel(writer, index=False, sheet_name='Sheet1')
-        stream.seek(0)
-
-        return f'Data exported to {filename}', dcc.send_bytes(stream.getvalue(), filename=filename)
-
-    return 'Click the button to export data to Excel.', None
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
