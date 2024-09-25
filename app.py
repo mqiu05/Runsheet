81a0e890a582bb884fb05c0f597e1aea6850e779
Michelle Qiu <michelleqiu05@gmail.com>
2024-09-25 12:21:42 -0400

    CODE WORKS THANK THE LORD

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dash import Dash, html, dcc, Output, Input
 
-app = Dash(__name__)
+app = Dash(__name__, suppress_callback_exceptions=True)
 
 app.layout = html.Div(children=[
     html.Button(
@@ -13,7 +13,23 @@ app.layout = html.Div(children=[
             'z-index': 2, 'text-decoration': 'underline'
         }
     ),
-    html.Div(id='general-section', style={'display': 'none'}),
+    html.Div(id='general-section', style={'display': 'none'}),  # Div that will be toggled
+
+    # Input elements are always present but initially hidden
+    html.Div(id='session-inputs', children=[
+        html.Div('Session Number', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Input(placeholder='Session Number', type='number', value='', id='session', style={'width': '100%'}),
+        html.Div('Date', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.DatePickerSingle(id='date-picker'),
+        html.Div('Venue', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Dropdown(['Purdue GP', 'Venue 2', 'Venue 3'], placeholder='Venue', id='venue'),
+        html.Div('Event', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Dropdown(['Skid', 'Brake', 'Autox'], placeholder='Event', id='event'),
+        html.Div('Driver', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Dropdown(['Iancarlo', 'Simon', 'Troy'], placeholder='Driver', id='driver'),
+        html.Div('Weight', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Input(placeholder='Weight', type='number', value='', id='weight', style={'width': '100%'}),
+    ], style={'display': 'none'}),  # Initially hidden
 
     html.Button(
         'Tires',
@@ -27,42 +43,38 @@ app.layout = html.Div(children=[
     ),
     html.Div(id='output-tires', style={'position': 'relative'}),
 
-    html.Button(
-        'Notes',
-        id='notes-button',
-        n_clicks=0,
-        style={
-            'outline': 'none', 'border': 'none', 'background-color': 'white',
-            'color': 'black', 'padding': '10px 20px', 'font-size': '25px', 'cursor': 'pointer',
-            'z-index': 2, 'text-decoration': 'underline'
-        }
-    ),
-    html.Div(id='notes-section', style={'display': 'none'}),  # Initially hidden
+    html.Button('Notes', id='notes-button', n_clicks=0,
+                style={'outline': 'none', 'border': 'none', 'background-color': 'white',
+                       'color': 'black', 'padding': '10px 20px', 'font-size': '25px', 'cursor': 'pointer', 'z-index': 2,
+                       'text-decoration': 'underline'}
+                ),
+
+    # Notes inputs always present but hidden
+    html.Div(id='notes-inputs', children=[
+        html.Div('Driver Notes', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Textarea(placeholder='Driver Notes', value='', style={'width': '100%'}, id='driver-notes'),
+        html.Div('Faults', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Textarea(placeholder='Faults', value='', style={'width': '100%'}, id='faults'),
+        html.Div('Improvements', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Textarea(placeholder='Improvements', value='', style={'width': '100%'}, id='improvements'),
+        html.Div('Misc.', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
+        dcc.Textarea(placeholder='Misc. Notes', value='', style={'width': '100%'}, id='misc-notes'),
+    ], style={'display': 'none'}),
 
     html.Button('Clear', id='clear-button', style={'margin-top': '20px'}),
 ])
 
+
 @app.callback(
     Output('general-section', 'children'),
+    Output('session-inputs', 'style'),
     Input('general-button', 'n_clicks')
 )
-
 def reveal_general(n_clicks):
     if n_clicks % 2 == 1:
-        return html.Div([
-            html.Div('Session Number', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Input(placeholder='Session Number', type='number', value='', id='session', style={'width': '100%'}),
-            html.Div('Date', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.DatePickerSingle(id='date-picker'),
-            html.Div('Venue', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Dropdown(['Purdue GP', 'Venue 2', 'Venue 3'], placeholder='Venue', id='venue'),
-            html.Div('Event', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Dropdown(['Skid', 'Brake', 'Autox'], placeholder='Event', id='event'),
-            html.Div('Driver', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Dropdown(['Iancarlo', 'Simon', 'Troy'], placeholder='Driver', id='driver'),
-            html.Div('Weight', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Input(placeholder='Weight', type='number', value='', id='weight', style={'width': '100%'}),
-        ])
+        return 'General Section Revealed', {'display': 'block'}
+    else:
+        return '', {'display': 'none'}
 
 
 @app.callback(
@@ -77,7 +89,7 @@ def reveal_tire(n_clicks):
                 style={
                     'position': 'relative',  # Keep within the flow
                     'margin': '0 auto', 'display': 'block',  # Center image horizontally
-                    'max-width': '70%', 'height': 'relative', 'margin-top':'100px'  # Responsive width and height
+                    'max-width': '70%', 'height': 'relative', 'margin-top': '100px'  # Responsive width and height
                 }
             ),
             # Add the four boxes to the corners of the screen
@@ -107,7 +119,7 @@ def reveal_tire(n_clicks):
                             dcc.Input(type='text', placeholder='', style={'width': '100%'})
                         ],
                         style={
-                            'position': 'absolute', 'margin-top': '40px','margin-left':'300px', 'left': '5%',
+                            'position': 'absolute', 'margin-top': '40px', 'margin-left': '300px', 'left': '5%',
                             'display': 'grid', 'grid-template-columns': 'repeat(4, 1fr)', 'gap': '10px',
                             'width': '15%', 'border': '2px solid black', 'padding': '10px',
                             'background-color': 'white'
@@ -137,7 +149,7 @@ def reveal_tire(n_clicks):
                             dcc.Input(type='text', placeholder='', style={'width': '100%'})
                         ],
                         style={
-                            'position': 'absolute', 'margin-top': '40px','margin-right':'300px', 'right': '5%',
+                            'position': 'absolute', 'margin-top': '40px', 'margin-right': '300px', 'right': '5%',
                             'display': 'grid', 'grid-template-columns': 'repeat(4, 1fr)', 'gap': '10px',
                             'width': '15%', 'border': '2px solid black', 'padding': '10px',
                             'background-color': 'white'
@@ -167,7 +179,7 @@ def reveal_tire(n_clicks):
                             dcc.Input(type='text', placeholder='', style={'width': '100%'})
                         ],
                         style={
-                              'position': 'absolute', 'bottom': '25%', 'left': '5%','margin-left':'300px',
+                            'position': 'absolute', 'bottom': '25%', 'left': '5%', 'margin-left': '300px',
                             'display': 'grid', 'grid-template-columns': 'repeat(4, 1fr)', 'gap': '10px',
                             'width': '15%', 'border': '2px solid black', 'padding': '10px',
                             'background-color': 'white'
@@ -197,7 +209,7 @@ def reveal_tire(n_clicks):
                             dcc.Input(type='text', placeholder='', style={'width': '100%'})
                         ],
                         style={
-                            'position': 'absolute', 'bottom': '25%', 'right': '5%','margin-right':'300px',
+                            'position': 'absolute', 'bottom': '25%', 'right': '5%', 'margin-right': '300px',
                             'display': 'grid', 'grid-template-columns': 'repeat(4, 1fr)', 'gap': '10px',
                             'width': '15%', 'border': '2px solid black', 'padding': '10px',
                             'background-color': 'white'
@@ -212,25 +224,19 @@ def reveal_tire(n_clicks):
                 children=[html.Div('Tire Compound', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
                           dcc.Input(type='text', placeholder='', style={'width': '100%'}),
                           ],
-                style={'position': 'absolute', 'margin-top': '3000px}', 'margin-left': '1050px'})
+                style={'position': 'relative', 'margin-top': '3000px', 'margin-left': '1050px'})
         ])
 
+
 @app.callback(
-    Output('notes-section', 'children'),
+    Output('notes-inputs', 'style'),
     Input('notes-button', 'n_clicks')
 )
 def reveal_notes(n_clicks):
     if n_clicks % 2 == 1:
-        return html.Div([
-            html.Div('Driver Notes', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Textarea(placeholder='Driver Notes', value='', style={'width': '100%'}, id='driver-notes'),
-            html.Div('Faults', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Textarea(placeholder='Faults', value='', style={'width': '100%'}, id='faults'),
-            html.Div('Improvements', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Textarea(placeholder='Improvements', value='', style={'width': '100%'}, id='improvements'),
-            html.Div('Misc.', style={'grid-column': '1 / 3', 'font-weight': 'bold'}),
-            dcc.Textarea(placeholder='Misc. Notes', value='', style={'width': '100%'}, id='misc-notes'),
-        ])
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
 
 
 @app.callback(
@@ -249,6 +255,7 @@ def reveal_notes(n_clicks):
 def clear_inputs(n_clicks):
     if n_clicks:
         return '', None, '', '', '', '', '', '', '', ''
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
