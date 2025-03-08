c1366afb6117524a2163476adef9bc04f5d3552d
apokrovs <apokrovs@purdue.edu>
2025-03-07 23:33:19 -0500

    added tire wear + fixed column width

--- anya.py
+++ anya.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, dash_table, Output, Input, State
+from dash import Dash, html, dcc, dash_table, Output, Input, State, no_update
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 import pandas as pd
@@ -51,6 +51,23 @@ def create_tire_box(tire_label, tire_id_prefix, top, left):
             dcc.Input(type='number', id=f'{tire_id_prefix}-oTemp-after', style={'width': '100%'}),
             dcc.Input(type='number', id=f'{tire_id_prefix}-mTemp-after', style={'width': '100%'}),
             dcc.Input(type='number', id=f'{tire_id_prefix}-iTemp-after', style={'width': '100%'}),
+    # Tire wear
+            html.Div('Wear', style={'grid-column': '1 / 2', 'font-weight': 'bold'}),
+            html.Div('O', style={'text-align': 'center', 'font-weight': 'bold'}),
+            html.Div('M', style={'text-align': 'center', 'font-weight': 'bold'}),
+            html.Div('I', style={'text-align': 'center', 'font-weight': 'bold'}),
+
+            # Temps Before
+            html.Div('Before', style={'grid-column': '1 / 2'}),
+            dcc.Input(type='number', id=f'{tire_id_prefix}-oWear-before', style={'width': '100%'}),
+            dcc.Input(type='number', id=f'{tire_id_prefix}-mWear-before', style={'width': '100%'}),
+            dcc.Input(type='number', id=f'{tire_id_prefix}-iWear-before', style={'width': '100%'}),
+
+            # Temps After
+            html.Div('After', style={'grid-column': '1 / 2'}),
+            dcc.Input(type='number', id=f'{tire_id_prefix}-oWear-after', style={'width': '100%'}),
+            dcc.Input(type='number', id=f'{tire_id_prefix}-mWear-after', style={'width': '100%'}),
+            dcc.Input(type='number', id=f'{tire_id_prefix}-iWear-after', style={'width': '100%'}),
         ],
         style={
             'position': 'absolute',
@@ -205,6 +222,8 @@ app.layout = dbc.Container([
         Output('front-wing', 'value'),
         Output('under-tray', 'value'),
         Output('rear-wing', 'value'),
+
+        # FL Tire
         Output('fl-pressure-before', 'value'),
         Output('fl-pressure-after', 'value'),
         Output('fl-oTemp-before', 'value'),
@@ -213,6 +232,14 @@ app.layout = dbc.Container([
         Output('fl-oTemp-after', 'value'),
         Output('fl-mTemp-after', 'value'),
         Output('fl-iTemp-after', 'value'),
+        Output('fl-oWear-before', 'value'),
+        Output('fl-mWear-before', 'value'),
+        Output('fl-iWear-before', 'value'),
+        Output('fl-oWear-after', 'value'),
+        Output('fl-mWear-after', 'value'),
+        Output('fl-iWear-after', 'value'),
+
+        # FR Tire
         Output('fr-pressure-before', 'value'),
         Output('fr-pressure-after', 'value'),
         Output('fr-oTemp-before', 'value'),
@@ -221,6 +248,14 @@ app.layout = dbc.Container([
         Output('fr-oTemp-after', 'value'),
         Output('fr-mTemp-after', 'value'),
         Output('fr-iTemp-after', 'value'),
+        Output('fr-oWear-before', 'value'),
+        Output('fr-mWear-before', 'value'),
+        Output('fr-iWear-before', 'value'),
+        Output('fr-oWear-after', 'value'),
+        Output('fr-mWear-after', 'value'),
+        Output('fr-iWear-after', 'value'),
+
+        # RL Tire
         Output('rl-pressure-before', 'value'),
         Output('rl-pressure-after', 'value'),
         Output('rl-oTemp-before', 'value'),
@@ -229,6 +264,14 @@ app.layout = dbc.Container([
         Output('rl-oTemp-after', 'value'),
         Output('rl-mTemp-after', 'value'),
         Output('rl-iTemp-after', 'value'),
+        Output('rl-oWear-before', 'value'),
+        Output('rl-mWear-before', 'value'),
+        Output('rl-iWear-before', 'value'),
+        Output('rl-oWear-after', 'value'),
+        Output('rl-mWear-after', 'value'),
+        Output('rl-iWear-after', 'value'),
+
+        # RR Tire
         Output('rr-pressure-before', 'value'),
         Output('rr-pressure-after', 'value'),
         Output('rr-oTemp-before', 'value'),
@@ -237,6 +280,14 @@ app.layout = dbc.Container([
         Output('rr-oTemp-after', 'value'),
         Output('rr-mTemp-after', 'value'),
         Output('rr-iTemp-after', 'value'),
+        Output('rr-oWear-before', 'value'),
+        Output('rr-mWear-before', 'value'),
+        Output('rr-iWear-before', 'value'),
+        Output('rr-oWear-after', 'value'),
+        Output('rr-mWear-after', 'value'),
+        Output('rr-iWear-after', 'value'),
+
+        # Other Fields
         Output('tire-compound', 'value'),
         Output('tire-set', 'value'),
         Output('front-spring-rate', 'value'),
@@ -246,7 +297,7 @@ app.layout = dbc.Container([
         Output('faults', 'value'),
         Output('improvements', 'value'),
         Output('misc', 'value'),
-    ] ,
+    ],
     Input('clear-button', 'n_clicks'),
     prevent_initial_call=True
 )
@@ -255,9 +306,21 @@ def clear_inputs(n_clicks):
         '',  # Session Number
         None, None, None, None, None,  # General
         None, None, None,  # Aero
-        '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
-         '','','','','', '', None, None, None, None,  # Suspension
-        '', '', ''  # Notes
+
+        # FL Tire
+        '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+
+        # FR Tire
+        '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+
+        # RL Tire
+        '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+
+        # RR Tire
+        '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+
+        # Suspension and Notes
+        '', '', None, None, None, None, '', '', ''
     )
 
 
@@ -283,6 +346,8 @@ def clear_inputs(n_clicks):
         State('event', 'value'),
         State('driver', 'value'),
         State('weight', 'value'),
+
+        # FL Tire
         State('fl-pressure-before', 'value'),
         State('fl-pressure-after', 'value'),
         State('fl-oTemp-before', 'value'),
@@ -291,6 +356,14 @@ def clear_inputs(n_clicks):
         State('fl-oTemp-after', 'value'),
         State('fl-mTemp-after', 'value'),
         State('fl-iTemp-after', 'value'),
+        State('fl-oWear-before', 'value'),
+        State('fl-mWear-before', 'value'),
+        State('fl-iWear-before', 'value'),
+        State('fl-oWear-after', 'value'),
+        State('fl-mWear-after', 'value'),
+        State('fl-iWear-after', 'value'),
+
+        # FR Tire
         State('fr-pressure-before', 'value'),
         State('fr-pressure-after', 'value'),
         State('fr-oTemp-before', 'value'),
@@ -299,6 +372,14 @@ def clear_inputs(n_clicks):
         State('fr-oTemp-after', 'value'),
         State('fr-mTemp-after', 'value'),
         State('fr-iTemp-after', 'value'),
+        State('fr-oWear-before', 'value'),
+        State('fr-mWear-before', 'value'),
+        State('fr-iWear-before', 'value'),
+        State('fr-oWear-after', 'value'),
+        State('fr-mWear-after', 'value'),
+        State('fr-iWear-after', 'value'),
+
+        # RL Tire
         State('rl-pressure-before', 'value'),
         State('rl-pressure-after', 'value'),
         State('rl-oTemp-before', 'value'),
@@ -307,6 +388,14 @@ def clear_inputs(n_clicks):
         State('rl-oTemp-after', 'value'),
         State('rl-mTemp-after', 'value'),
         State('rl-iTemp-after', 'value'),
+        State('rl-oWear-before', 'value'),
+        State('rl-mWear-before', 'value'),
+        State('rl-iWear-before', 'value'),
+        State('rl-oWear-after', 'value'),
+        State('rl-mWear-after', 'value'),
+        State('rl-iWear-after', 'value'),
+
+        # RR Tire
         State('rr-pressure-before', 'value'),
         State('rr-pressure-after', 'value'),
         State('rr-oTemp-before', 'value'),
@@ -315,6 +404,14 @@ def clear_inputs(n_clicks):
         State('rr-oTemp-after', 'value'),
         State('rr-mTemp-after', 'value'),
         State('rr-iTemp-after', 'value'),
+        State('rr-oWear-before', 'value'),
+        State('rr-mWear-before', 'value'),
+        State('rr-iWear-before', 'value'),
+        State('rr-oWear-after', 'value'),
+        State('rr-mWear-after', 'value'),
+        State('rr-iWear-after', 'value'),
+
+        # Other Fields
         State('tire-compound', 'value'),
         State('tire-set', 'value'),
         State('front-wing', 'value'),
@@ -327,6 +424,8 @@ def clear_inputs(n_clicks):
         State('faults', 'value'),
         State('improvements', 'value'),
         State('misc', 'value'),
+
+        # Existing Data
         State('general-table', 'columns'),
         State('general-table', 'data'),
         State('tire-table', 'columns'),
@@ -340,20 +439,24 @@ def clear_inputs(n_clicks):
     ],
     prevent_initial_call=True
 )
+
 def save_data(n_clicks, session, date, venue, event, driver, weight,
-              fl_pressure_before,
-              fl_pressure_after, fl_oTemp_before,
-              fl_mTemp_before, fl_iTemp_before,
-              fl_oTemp_after, fl_mTemp_after, fl_iTemp_after,
-              fr_pressure_before,fr_pressure_after, fr_oTemp_before,
-              fr_mTemp_before, fr_iTemp_before,
-              fr_oTemp_after, fr_mTemp_after, fr_iTemp_after,
-              rl_pressure_before, rl_pressure_after, rl_oTemp_before,
-              rl_mTemp_before, rl_iTemp_before,
-              rl_oTemp_after, rl_mTemp_after, rl_iTemp_after,
-              rr_pressure_before, rr_pressure_after, rr_oTemp_before,
-              rr_mTemp_before, rr_iTemp_before,
-              rr_oTemp_after, rr_mTemp_after, rr_iTemp_after,
+              fl_pressure_before, fl_pressure_after, fl_oTemp_before, fl_mTemp_before, fl_iTemp_before,
+              fl_oTemp_after, fl_mTemp_after, fl_iTemp_after, fl_oWear_before, fl_mWear_before, fl_iWear_before,
+              fl_oWear_after, fl_mWear_after, fl_iWear_after,
+
+              fr_pressure_before, fr_pressure_after, fr_oTemp_before, fr_mTemp_before, fr_iTemp_before,
+              fr_oTemp_after, fr_mTemp_after, fr_iTemp_after, fr_oWear_before, fr_mWear_before, fr_iWear_before,
+              fr_oWear_after, fr_mWear_after, fr_iWear_after,
+
+              rl_pressure_before, rl_pressure_after, rl_oTemp_before, rl_mTemp_before, rl_iTemp_before,
+              rl_oTemp_after, rl_mTemp_after, rl_iTemp_after, rl_oWear_before, rl_mWear_before, rl_iWear_before,
+              rl_oWear_after, rl_mWear_after, rl_iWear_after,
+
+              rr_pressure_before, rr_pressure_after, rr_oTemp_before, rr_mTemp_before, rr_iTemp_before,
+              rr_oTemp_after, rr_mTemp_after, rr_iTemp_after, rr_oWear_before, rr_mWear_before, rr_iWear_before,
+              rr_oWear_after, rr_mWear_after, rr_iWear_after,
+
               tire_compound, tire_set,
               front_wing, under_tray, rear_wing,
               front_spring, rear_spring, right_arb, left_arb,
@@ -369,11 +472,11 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
     # Session is mandatory: if empty, display error dialog instead of saving
     if not session:
         return (
-            PreventUpdate, PreventUpdate,  # general table
-            PreventUpdate, PreventUpdate,  # tire table
-            PreventUpdate, PreventUpdate,  # aero table
-            PreventUpdate, PreventUpdate,  # suspension table
-            PreventUpdate, PreventUpdate,  # notes table
+            no_update, no_update,  # general table
+            no_update, no_update,  # tire table
+            no_update, no_update,  # aero table
+            no_update, no_update,  # suspension table
+            no_update, no_update,  # notes table
             True  # session-error => displayed
         )
 
@@ -383,6 +486,7 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
     event = event or ''
     driver = driver or ''
     weight = weight or ''
+
     fl_pressure_before = fl_pressure_before or ''
     fl_pressure_after = fl_pressure_after or ''
     fl_oTemp_before = fl_oTemp_before or ''
@@ -391,6 +495,13 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
     fl_oTemp_after = fl_oTemp_after or ''
     fl_mTemp_after = fl_mTemp_after or ''
     fl_iTemp_after = fl_iTemp_after or ''
+    fl_oWear_before = fl_oWear_before or ''
+    fl_mWear_before = fl_mWear_before or ''
+    fl_iWear_before = fl_iWear_before or ''
+    fl_oWear_after = fl_oWear_after or ''
+    fl_mWear_after = fl_mWear_after or ''
+    fl_iWear_after = fl_iWear_after or ''
+
     fr_pressure_before = fr_pressure_before or ''
     fr_pressure_after = fr_pressure_after or ''
     fr_oTemp_before = fr_oTemp_before or ''
@@ -399,6 +510,13 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
     fr_oTemp_after = fr_oTemp_after or ''
     fr_mTemp_after = fr_mTemp_after or ''
     fr_iTemp_after = fr_iTemp_after or ''
+    fr_oWear_before = fr_oWear_before or ''
+    fr_mWear_before = fr_mWear_before or ''
+    fr_iWear_before = fr_iWear_before or ''
+    fr_oWear_after = fr_oWear_after or ''
+    fr_mWear_after = fr_mWear_after or ''
+    fr_iWear_after = fr_iWear_after or ''
+
     rl_pressure_before = rl_pressure_before or ''
     rl_pressure_after = rl_pressure_after or ''
     rl_oTemp_before = rl_oTemp_before or ''
@@ -407,14 +525,28 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
     rl_oTemp_after = rl_oTemp_after or ''
     rl_mTemp_after = rl_mTemp_after or ''
     rl_iTemp_after = rl_iTemp_after or ''
-    rr_pressure_before= rr_pressure_before or ''
+    rl_oWear_before = rl_oWear_before or ''
+    rl_mWear_before = rl_mWear_before or ''
+    rl_iWear_before = rl_iWear_before or ''
+    rl_oWear_after = rl_oWear_after or ''
+    rl_mWear_after = rl_mWear_after or ''
+    rl_iWear_after = rl_iWear_after or ''
+
+    rr_pressure_before = rr_pressure_before or ''
     rr_pressure_after = rr_pressure_after or ''
     rr_oTemp_before = rr_oTemp_before or ''
     rr_mTemp_before = rr_mTemp_before or ''
     rr_iTemp_before = rr_iTemp_before or ''
     rr_oTemp_after = rr_oTemp_after or ''
     rr_mTemp_after = rr_mTemp_after or ''
-    rr_iTemp_after = rr_iTemp_after or '' # If blank or None, store as ''
+    rr_iTemp_after = rr_iTemp_after or ''
+    rr_oWear_before = rr_oWear_before or ''
+    rr_mWear_before = rr_mWear_before or ''
+    rr_iWear_before = rr_iWear_before or ''
+    rr_oWear_after = rr_oWear_after or ''
+    rr_mWear_after = rr_mWear_after or ''
+    rr_iWear_after = rr_iWear_after or ''
+
     tire_compound = tire_compound or ''
     tire_set = tire_set or ''
     front_wing = front_wing or ''
@@ -449,6 +581,7 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
 
     # TIRE TABLE
     new_tire_row = {
+        # Front Left Tire
         'FL Pressure Before': fl_pressure_before,
         'FL Pressure After': fl_pressure_after,
         'FL Outer Temp Before': fl_oTemp_before,
@@ -457,7 +590,14 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
         'FL Outer Temp After': fl_oTemp_after,
         'FL Middle Temp After': fl_mTemp_after,
         'FL Inner Temp After': fl_iTemp_after,
+        'FL Outer Wear Before': fl_oWear_before,
+        'FL Middle Wear Before': fl_mWear_before,
+        'FL Inner Wear Before': fl_iWear_before,
+        'FL Outer Wear After': fl_oWear_after,
+        'FL Middle Wear After': fl_mWear_after,
+        'FL Inner Wear After': fl_iWear_after,
 
+        # Front Right Tire
         'FR Pressure Before': fr_pressure_before,
         'FR Pressure After': fr_pressure_after,
         'FR Outer Temp Before': fr_oTemp_before,
@@ -466,7 +606,14 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
         'FR Outer Temp After': fr_oTemp_after,
         'FR Middle Temp After': fr_mTemp_after,
         'FR Inner Temp After': fr_iTemp_after,
+        'FR Outer Wear Before': fr_oWear_before,
+        'FR Middle Wear Before': fr_mWear_before,
+        'FR Inner Wear Before': fr_iWear_before,
+        'FR Outer Wear After': fr_oWear_after,
+        'FR Middle Wear After': fr_mWear_after,
+        'FR Inner Wear After': fr_iWear_after,
 
+        # Rear Left Tire
         'RL Pressure Before': rl_pressure_before,
         'RL Pressure After': rl_pressure_after,
         'RL Outer Temp Before': rl_oTemp_before,
@@ -475,7 +622,14 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
         'RL Outer Temp After': rl_oTemp_after,
         'RL Middle Temp After': rl_mTemp_after,
         'RL Inner Temp After': rl_iTemp_after,
+        'RL Outer Wear Before': rl_oWear_before,
+        'RL Middle Wear Before': rl_mWear_before,
+        'RL Inner Wear Before': rl_iWear_before,
+        'RL Outer Wear After': rl_oWear_after,
+        'RL Middle Wear After': rl_mWear_after,
+        'RL Inner Wear After': rl_iWear_after,
 
+        # Rear Right Tire
         'RR Pressure Before': rr_pressure_before,
         'RR Pressure After': rr_pressure_after,
         'RR Outer Temp Before': rr_oTemp_before,
@@ -484,9 +638,18 @@ def save_data(n_clicks, session, date, venue, event, driver, weight,
         'RR Outer Temp After': rr_oTemp_after,
         'RR Middle Temp After': rr_mTemp_after,
         'RR Inner Temp After': rr_iTemp_after,
+        'RR Outer Wear Before': rr_oWear_before,
+        'RR Middle Wear Before': rr_mWear_before,
+        'RR Inner Wear Before': rr_iWear_before,
+        'RR Outer Wear After': rr_oWear_after,
+        'RR Middle Wear After': rr_mWear_after,
+        'RR Inner Wear After': rr_iWear_after,
+
+        # Tire Compound & Set
         'Tire Compound': tire_compound,
         'Tire Set': tire_set,
     }
+
     if not tire_data:
         tire_df = pd.DataFrame([new_tire_row])
     else:
@@ -574,11 +737,17 @@ def export_data(n_clicks, general_data, tire_data, aero_data, susp_data, notes_d
     notes_df = pd.DataFrame(notes_data)
 
     with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
-        general_df.to_excel(writer, sheet_name='General', index=False)
-        tire_df.to_excel(writer, sheet_name='Tires', index=False)
-        aero_df.to_excel(writer, sheet_name='Aero', index=False)
-        susp_df.to_excel(writer, sheet_name='Suspension', index=False)
-        notes_df.to_excel(writer, sheet_name='Notes', index=False)
+        for df, sheet_name in zip(
+                [general_df, tire_df, aero_df, susp_df, notes_df],
+                ['General', 'Tires', 'Aero', 'Suspension', 'Notes']
+        ):
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+            # 🔥 Auto-adjust column widths
+            worksheet = writer.sheets[sheet_name]
+            for col_idx, col in enumerate(df.columns):
+                max_length = max(df[col].astype(str).map(len).max(), len(col)) + 2
+                worksheet.column_dimensions[worksheet.cell(1, col_idx + 1).column_letter].width = max_length
 
     return dcc.send_file(file_path)
 
